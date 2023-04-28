ba341dc1f6211775f22e3814c9c2278e070c7a55
bebestmaple <447349864@qq.com>
2023-04-28 17:44:05 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,14 +17,14 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "file",
+  title: process.env.Title || "file",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
-  maxItems: 100,
+  maxItems: 200,
 
   // [OPTIONAL] We use Google Fonts natively for font customisations.
   // You can check and generate the required links and names at https://fonts.google.com.
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  protectedRoutes: (process.env.ProtectedRoutes || []).concat( ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route']),
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
